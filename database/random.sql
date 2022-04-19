573e586b868bc1f50251d20a47b0da411177276d
Tan Ha <79884390+tandha@users.noreply.github.com>
2022-04-19 02:16:13 -0700

    query for product style route working

--- database/random.sql
+++ database/random.sql
@@ -15,41 +15,57 @@
 -- SELECT * FROM productoverview.styles limit 10;
 
 
-select
-	json_build_object(
-			'product_id', '1', 'results', json_agg(
-				json_build_object(
-					'style_id', s.id,
-					'name', s.name,
-					'original_price', s.original_price,
-					'sale_price', s.sale_price,
-					'default?', s.default_style,
-					'photos', photos,
-					'skus', skus
-				)
-			)
-	) styles
-from productoverview.styles s
-left join (
-	select styleId,
-				 json_agg(
-					 	json_build_object(
-							 'thumbnail_url', p.thumbnail_url,
-							 'url', p.url
-						 )
-				 ) photos,
-				--  from productoverview.photos p group by 1,
-				 json_agg(
-					 k.id,
-					 json_build_object(
-						 'quantity', k.quantity,
-						 'size', k.size
-					 )
-				 ) skus
-				 from productoverview.photos p, productoverview.skus
-				 group by 1
-				--  where p.id = k.id
-) p on s.id = p.styleId where s.productId = 1
+-- select
+-- 	json_build_object(
+-- 			'product_id', '1', 'results', json_agg(
+-- 				json_build_object(
+-- 					'style_id', s.id,
+-- 					'name', s.name,
+-- 					'original_price', s.original_price,
+-- 					'sale_price', s.sale_price,
+-- 					'default?', s.default_style,
+-- 					'photos', photos,
+-- 					'skus', skus
+-- 				)
+-- 			)
+-- 	) styles
+-- from productoverview.styles s
+-- left join (
+-- 	select styleId,
+-- 				 json_agg(
+-- 					 	json_build_object(
+-- 							 'thumbnail_url', p.thumbnail_url,
+-- 							 'url', p.url
+-- 						 )
+-- 				 ) photos
+-- 				 from productoverview.photos p
+-- 				 left join (
+-- 					 SELECT
+-- 					 	styleId,
+-- 						 json_agg(
+-- 							 json_build_object(
+-- 								 'quantity', k.quantity,
+-- 								 'size', k.size
+-- 							 )
+-- 						 ) skus
+-- 						 from productoverview.skus k
+-- 						 group by 1
+-- 				 ) k on p.styleId = k.styleId
+-- 				 group by styleId
+-- ) p on p.styleId = k.styleId;
+
+
+-- 				 json_agg(
+-- 					 k.id,
+-- 					 json_build_object(
+-- 						 'quantity', k.quantity,
+-- 						 'size', k.size
+-- 					 )
+-- 				 ) skus
+-- 				 from productoverview.photos p, productoverview.skus
+-- 				 group by 1
+-- 				--  where p.id = k.id
+-- ) p on s.id = p.styleId where s.productId = 1
 
 
 -- from productoverview.photos p group by 1
@@ -63,3 +79,39 @@ left join (
 -- )
 
 -- ) p on s.id = p.styleId where s.productId = 1
+
+
+
+
+select json_build_object(
+	'product', '1',
+	'results', json_agg(
+		json_build_object(
+			'style_id', s.id,
+			'name', s.name,
+			'original_price', s.original_price,
+			'sale_price', s.sale_price,
+			'default?', s.default_style,
+			'photos', (select (json_agg(
+				json_build_object(
+					'thumbnail_url', p.thumbnail_url,
+					'url', p.url
+				)
+			)
+			) as photos from productoverview.photos p where p.styleId = s.id
+
+			),
+			'skus', (select json_object_agg(
+					k.id, json_build_object(
+						'quantity', k.quantity,
+						'size', k.size
+					)
+				) from productoverview.skus k where k.styleId = s.id
+			)
+
+			)
+		)
+	)
+from productoverview.styles s where s.productId = 1
+
+-- select count(quantity), size from productoverview.skus group by size;
