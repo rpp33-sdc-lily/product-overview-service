7ca1527184b8e4785aabe4bd8e2af6f8b6d280de
Tan Ha <79884390+tandha@users.noreply.github.com>
2022-05-24 14:56:29 -0700

    updated loader.io stuff

--- server/index.js
+++ server/index.js
@@ -27,8 +27,8 @@ const DEFAULT_EXPIRATION = 3600;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/loaderio-d01ac8bde8314c2fc7b5b47f9f8899e2.txt', (req, res) => {
-	res.sendFile('/home/ubuntu/product-overview-service/loaderio-d01ac8bde8314c2fc7b5b47f9f8899e2.txt');
+app.get('/loaderio-3c94c7000073677355445e3d9d46086e', (req, res) => {
+	res.send('loaderio-3c94c7000073677355445e3d9d46086e');
 	// res.sendFile('/Users/tanha/RPP33/SDC/product-overview-service/loaderio-07a1310218300c2fc487392e6596ba3b.txt');
 });
 
